beeb3b99796511b2e7e13955429974c4df7af5be
YUNUS EMRE TASCİ <yunusemretasci092@gmail.com>
2023-02-20 12:17:34 +0300

    login

--- users/views.py
+++ users/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.forms import UserCreationForm
+from .forms import UserForm,LoginForm
 
 # Create your views here.
 def register(request):
-    form=UserCreationForm()
+    # form=UserCreationForm()
+    form=UserForm()
     
     if request.method=="POST":
-        form=UserCreationForm(request.POST)
+        # form=UserCreationForm(request.POST)
+        form=UserForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect("home")
@@ -16,3 +19,12 @@ def register(request):
     }
     
     return render(request,"users/register.html",context)
+
+def user_login(request):
+    form=LoginForm()
+    
+    context={
+        "form":form
+    }
+    
+    return render(request,"users/login.html",context)
