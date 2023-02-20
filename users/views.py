8dd519e7d76818e102729a6930665cb194e03fe3
YUNUS EMRE TASCİ <yunusemretasci092@gmail.com>
2023-02-20 12:49:41 +0300

    logout-register-login

--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.forms import UserCreationForm
 from .forms import UserForm,LoginForm
+from django.contrib.auth import login,logout,authenticate
 
 # Create your views here.
 def register(request):
@@ -11,7 +12,14 @@ def register(request):
         # form=UserCreationForm(request.POST)
         form=UserForm(request.POST)
         if form.is_valid():
-            form.save()
+            user=form.save()
+            login(request,user)
+            
+            # username = form.cleaned_data.get("username")
+            # password = form.cleaned_data.get("password2")
+            # user = authenticate(username=username, password=password)
+            # login(request, user)
+            
             return redirect("home")
     
     context={
@@ -23,8 +31,19 @@ def register(request):
 def user_login(request):
     form=LoginForm()
     
+    if request.method=="POST":
+        form=LoginForm(request.POST)
+        if form.is_valid:
+            user=form.get_user()
+            login(request,user)
+            return redirect("home")
+    
     context={
         "form":form
     }
     
     return render(request,"users/login.html",context)
+
+def user_logout(request):
+    logout(request)
+    return redirect("home")
