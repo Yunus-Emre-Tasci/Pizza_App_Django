9775d71cb63594483d604b6fb33e271c100d79cf
YUNUS EMRE TASCİ <yunusemretasci092@gmail.com>
2023-02-23 16:13:01 +0300

    update_orders

--- pizzas/urls.py
+++ pizzas/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import home,pizzas,order_view
+from .views import home,pizzas,order_view,my_orders,update_order_view
 
 urlpatterns = [
     path("",home,name="home"),
     path("pizzas/",pizzas,name="pizzas"),
     path("pizzas/<int:id>/",order_view,name="order"),
+    path("update_orders_view/<int:id>/",update_order_view,name="update_orders"),
+    path("my_orders/",my_orders,name="my_orders"),
 ]
--- pizzas/views.py
+++ pizzas/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render,redirect
-from .models import Pizza
+from .models import Pizza,Order
 from .forms import PizzaForm
 
 # Create your views here.
@@ -35,4 +35,21 @@ def order_view(request,id):
     return render(request,"pizzas/order.html",context)
 
 def my_orders(request):
-    return render(request,"pizzas/my_orders.html")
+    orders=Order.objects.filter(user=request.user)
+    
+    context={
+        "orders":orders,
+    }
+    
+    return render(request,"pizzas/my_orders.html",context)
+
+def update_order_view(request,id):
+    order=Order.objects.get(id=id)
+    pizza=Pizza.objects.get(id=order.pizza.id)
+    
+    context={
+        "order":order,
+        "pizza":pizza
+    }
+    
+    return render(request,"pizzas/update_order.html",context)
